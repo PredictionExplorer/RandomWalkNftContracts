9ef07a7556dcb5ec07085c96efa1cfd4c9a41d24
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2021-10-08 12:20:05 -0500

    Fix mintMany

--- mintMany.sol
+++ mintMany.sol
@@ -1,25 +1,28 @@
 import "./randomWalkNFT.sol";
 
-contract MintMany is Ownable {
+contract MintMany {
 
-    randomWalkNFT nftAddress;
+    RandomWalkNFT nftAddress;
+    address target;
 
     constructor (address nftAddr) {
-        nftAddress = randomWalkNFT(nftAddr);
+        nftAddress = RandomWalkNFT(nftAddr);
     }
 
-    function mintMultiple(uint256 num) public onlyOwner {
+    function mintMultiple(uint256 num) public payable {
+        target = msg.sender;
         for (uint256 i = 0; i < num; i++) {
             uint256 mintPrice = nftAddress.getMintPrice();
             nftAddress.mint{value: mintPrice}();
         }
     }
 
-    function approveAll(address operator) public onlyOwner {
-        nftAddress.setApprovalForAll(operator, true);
-    }
-
     function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) public returns(bytes4) {
+        nftAddress.safeTransferFrom(
+            address(this),
+            target,
+            tokenId
+        );
         bytes4 _ERC721_RECEIVED = 0x150b7a02;
         return _ERC721_RECEIVED;
     }
