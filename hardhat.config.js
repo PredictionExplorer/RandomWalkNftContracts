ea18cb4e4736aa2554d8f44ee50ebe28414afdce
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2021-10-15 01:20:23 -0500

    Add support for Rinkeby Arbitrum for deploying

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
       url: `https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161`,
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    rinkarby: {
+      url: `https://rinkeby.arbitrum.io/rpc`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   etherscan: {
     apiKey: process.env.API_KEY,
