c1015f205482222da6a4749ad480654172020735
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2021-10-15 14:39:53 -0500

    Add sale test

--- test/randomWalkNFT.js
+++ test/randomWalkNFT.js
@@ -33,6 +33,72 @@ describe("RandomWalkNFT contract", function () {
     hardhatMarketplace = await Marketplace.deploy(hardhatRandomWalkNFT.address);
   });
 
+  it("Sale should work", async function () {
+    mintPrice = await hardhatRandomWalkNFT.getMintPrice();
+    await hardhatRandomWalkNFT.mint({value: mintPrice});
+    await expect(hardhatMarketplace.makeSellOffer(0, ethers.utils.parseEther('0.123'))).to.be.revertedWith('ERC721: transfer caller is not owner nor approved');;
+    await hardhatRandomWalkNFT.setApprovalForAll(hardhatMarketplace.address, true);
+    let price = ethers.utils.parseEther('0.123');
+    await hardhatMarketplace.makeSellOffer(0, price);
+    initial_balance = await ethers.provider.getBalance(owner.address);
+    await expect(hardhatMarketplace.connect(addr1).acceptBuyOffer(0)).to.be.revertedWith("Must be a buy offer");
+    await expect(hardhatMarketplace.connect(addr1).acceptSellOffer(0, {value: price.sub(1)})).to.be.revertedWith("Incorrect value sent.");
+    await expect(hardhatMarketplace.connect(addr1).acceptSellOffer(0, {value: price.add(1)})).to.be.revertedWith("Incorrect value sent.");
+    await hardhatMarketplace.connect(addr1).acceptSellOffer(0, {value: price});
+    await expect(hardhatMarketplace.connect(addr1).acceptSellOffer(0, {value: price})).to.be.revertedWith("Offer must be active");
+    expect(initial_balance.add(price).eq(await ethers.provider.getBalance(owner.address))).to.equal(true);
+    expect(await hardhatRandomWalkNFT.ownerOf(0)).to.equal(addr1.address);
+  });
+
+  it("Can't mint before sale is open", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const targetTimestamp = blockBefore.timestamp + 7 * 24 * 3600;
+    hardhatRandomWalkNFTweek = await RandomWalkNFT.deploy("", targetTimestamp);
+
+    mintPrice = await hardhatRandomWalkNFTweek.getMintPrice();
+    await expect(hardhatRandomWalkNFTweek.mint({value: mintPrice})).to.be.revertedWith('The sale is not open yet.');
+
+    await ethers.provider.send("evm_increaseTime", [7 * 24 * 3600 - 15]);
+    await ethers.provider.send("evm_mine");
+
+    await expect(hardhatRandomWalkNFTweek.mint({value: mintPrice})).to.be.revertedWith('The sale is not open yet.');
+    expect(await hardhatRandomWalkNFTweek.totalSupply()).to.equal(0);
+
+    await ethers.provider.send("evm_increaseTime", [60]);
+    await ethers.provider.send("evm_mine");
+
+    await hardhatRandomWalkNFTweek.mint({value: mintPrice});
+    expect(await hardhatRandomWalkNFTweek.totalSupply()).to.equal(1);
+  });
+
+  it("The minter withdraws and gets the money", async function () {
+    totalSpent = ethers.BigNumber.from("0");
+    for(i = 0; i < 100; i++) {
+      mintPrice = await hardhatRandomWalkNFT.getMintPrice();
+      totalSpent = totalSpent.add(mintPrice);
+      await hardhatRandomWalkNFT.mint({value: mintPrice});
+    }
+    expect(await hardhatRandomWalkNFT.lastMinter()).to.equal(owner.address);
+
+    refund = await ethers.provider.getBalance(hardhatRandomWalkNFT.address) / 2;
+    initial_balance = await ethers.provider.getBalance(owner.address);
+
+    await expect(hardhatRandomWalkNFT.withdraw()).to.be.revertedWith('Not enough time has elapsed.');
+
+    await ethers.provider.send("evm_increaseTime", [30 * 24 * 3600 - 15]);
+    await ethers.provider.send("evm_mine");
+
+    await expect(hardhatRandomWalkNFT.withdraw()).to.be.revertedWith('Not enough time has elapsed.');
+
+    await ethers.provider.send("evm_increaseTime", [30]);
+    await ethers.provider.send("evm_mine");
+
+    await expect(hardhatRandomWalkNFT.connect(addr1).withdraw()).to.be.revertedWith('Only last minter can withdraw.');
+
+    init_user_balance = await ethers.provider.getBalance(owner.address);
+  });
+
   it("Can't mint before sale is open", async function () {
     const blockNumBefore = await ethers.provider.getBlockNumber();
     const blockBefore = await ethers.provider.getBlock(blockNumBefore);
