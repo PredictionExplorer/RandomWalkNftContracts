a9735f000557be509e7104a495f1e8df7b345390
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2021-10-14 22:47:11 -0500

    Add mint test

--- test/randomWalkNFT.js
+++ test/randomWalkNFT.js
@@ -1,16 +1,56 @@
 const { expect } = require("chai");
 
+
 describe("RandomWalkNFT contract", function () {
+
+  let RandomWalkNFT;
+  let hardhatRandomWalkNFT;
+
+  let Marketplace;
+  let hardhatMarketplace;
+
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    RandomWalkNFT = await ethers.getContractFactory("RandomWalkNFT");
+    hardhatRandomWalkNFT = await RandomWalkNFT.deploy(0, 0);
+
+    Marketplace = await ethers.getContractFactory("Marketplace");
+    hardhatMarketplace = await Marketplace.deploy(hardhatRandomWalkNFT.address);
+  });
+
   it("Initial supply should be zero", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const RandomWalkNFT = await ethers.getContractFactory("RandomWalkNFT");
-
-    const hardhatRandomWalkNFT = await RandomWalkNFT.deploy(0, 0);
-
-    console.log("Taras 123");
-    console.log(RandomWalkNFT.address);
-
     expect(await hardhatRandomWalkNFT.totalSupply()).to.equal(0);
   });
+
+  it("Initial number of offers should be zero", async function () {
+    expect(await hardhatMarketplace.numOffers()).to.equal(0);
+  });
+
+  it("Minting should work", async function () {
+    mintPrice = await hardhatRandomWalkNFT.getMintPrice() - 0;
+    await hardhatRandomWalkNFT.mint({value: mintPrice});
+
+    mintPrice = await hardhatRandomWalkNFT.getMintPrice() - 1;
+    await expect(hardhatRandomWalkNFT.mint({value: mintPrice})).to.be.revertedWith('The value submitted with this transaction is too low.');
+
+    mintPrice = await hardhatRandomWalkNFT.getMintPrice() - 0;
+    await hardhatRandomWalkNFT.connect(addr1).mint({value: mintPrice});
+
+    mintPrice = await hardhatRandomWalkNFT.getMintPrice() - 0;
+    await hardhatRandomWalkNFT.connect(addr1).mint({value: mintPrice});
+
+    expect(await hardhatRandomWalkNFT.totalSupply()).to.equal(3);
+    expect(await hardhatRandomWalkNFT.balanceOf(owner.address)).to.equal(1);
+    expect(await hardhatRandomWalkNFT.balanceOf(addr1.address)).to.equal(2);
+    expect(await hardhatRandomWalkNFT.balanceOf(addr2.address)).to.equal(0);
+  });
+
+
 });
